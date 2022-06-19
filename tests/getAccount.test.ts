decdf90acfbd0404c24238c419c38417e3b0e9ef
Eight∞ <wangzndg@gmail.com>
2022-06-19 10:26:22 +0200

    test: add test for getAccount function

---
+++ tests/getAccount.test.ts
@@ -0,0 +1,22 @@
+import { Client } from "../src";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+	token: process.env.TOKEN,
+});
+
+test("Get Account", async () => {
+	const account = await client.getAccount();
+
+	if (!account) throw new Error("Account not found");
+
+	expect(account.username).toBe("NotReallyEight");
+	expect(account.credits).toBeGreaterThanOrEqual(0);
+	expect(account.joinedAt).toMatch(
+		/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/
+	);
+	expect(account.shipCount).toBeGreaterThanOrEqual(0);
+	expect(account.structureCount).toBeGreaterThanOrEqual(0);
+});
