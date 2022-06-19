444e9fbd5c57adb3fef1c1f975b2d108d8f79b4a
Eight∞ <wangzndg@gmail.com>
2022-06-19 10:26:39 +0200

    test: add test for getAvailableLoans function

---
+++ tests/getAvailableLoans.test.ts
@@ -0,0 +1,23 @@
+import { Client } from "../src";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new Client({
+	token: process.env.TOKEN,
+});
+
+test("Get Available Loans", async () => {
+	const response = await client.getAvailableLoans();
+
+	if (!response) throw new Error("Loans not found");
+
+	expect(response.loans).toBeTruthy();
+	for (let i = 0; i < response.loans.length; i++) {
+		expect(response.loans[i].amount).toBeGreaterThanOrEqual(0);
+		expect(typeof response.loans[i].collateralRequired).toBe("boolean");
+		expect(response.loans[i].rate).toBeGreaterThanOrEqual(0);
+		expect(response.loans[i].termInDays).toBeGreaterThanOrEqual(0);
+		expect(typeof response.loans[i].type).toBe("string");
+	}
+});
